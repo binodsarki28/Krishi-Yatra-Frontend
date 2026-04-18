719633843cab0925e93c9224d33dc9d3e286e112
binodsarki28 <binodsarki69@gmail.com>
2026-04-18 20:04:52 +0545

    fix: switch dynamic routes to Client render mode to enable Vercel build

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,35 +3,35 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'farmer/stocks/update/:slug',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
   },
   {
     path: 'farmer/stocks/details/:slug',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
   },
   {
     path: 'stock-detail/:slug',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
   },
   {
     path: 'demands',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender
   },
   {
     path: 'order/create/:slug',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
   },
   {
     path: 'delivery/track/:orderId',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
   },
   {
     path: 'farmer/orders/track/:orderId',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
   },
   {
     path: 'buyer/orders/track/:orderId',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
   },
   {
     path: '**',
